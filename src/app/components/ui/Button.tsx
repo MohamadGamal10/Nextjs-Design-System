01dc1a9d76f11978074e76bb8d4cf981587252cd
Mohamed Gamal <mohamadgamalbusiness@gmail.com>
2025-02-19 13:49:26 +0200

    before merge variants

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,13 +1,28 @@
 import { ButtonHTMLAttributes, ReactNode } from "react"
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+const buttonVariants = cva("rounded-md font-semibold block duration-200 ", {
+  variants: {
+    intent: {
+      primary: "bg-black text-white dark:bg-white dark:text-black",
+      outline: "bg-transparent text-black dark:text-white border border-gray-300",
+      destructive: "bg-red-600 text-black dark:bg-red-800 dark:text-white border border-gray-300",
+    }
+  },
+  defaultVariants: {
+    intent: "primary",
+  },
+})
+
+
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
   children: ReactNode;
   className?: string;
-  width?: "w-full" | "w-fit"
 }
-const Button = ({ children, className, width= "w-full", ...rest } : IProps) => {
+
+const Button = ({ children, className, intent, ...rest } : IProps) => {
   return (
-    <button className={`${className} ${width} rounded-lg text-white px-3 py-3 duration-200 font-medium` }{...rest}>
+    <button className={`${className}  ${buttonVariants({intent: intent})} px-3 py-3 ` }{...rest}>
       {children}
     </button>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@ import Button from "./components/ui/Button";
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
-      <h1 className="text-center text-4xl bg-white rounded-md text-black w-fit p-5 mx-auto font-sans">
+      <h1 className="text-center my-5 text-4xl bg-white rounded-md text-black w-fit p-5 mx-auto font-sans">
         Design System in Depth
       </h1>
-      <Button>Solid Button</Button>
+      <div className="space-y-3">
+      <Button>PRIMARY</Button>
+      <Button intent={"destructive"}>DESTRUCTIVE</Button>
+      <Button>SECONDARY</Button>
+      <Button intent={"outline"}>OUTLINE</Button>
+      <Button>GHOST</Button>
+      <Button>LINK</Button>
+      </div>
     </main>
   );
 }
